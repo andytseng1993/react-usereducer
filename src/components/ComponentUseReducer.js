832a5953e69c32a3f1e819a45b0c2c8a40111730
Andy Tseng <anby1611@gmail.com>
2022-03-30 08:02:56 +0000

    ACTIONS

--- src/components/ComponentUseReducer.js
+++ src/components/ComponentUseReducer.js
@@ -1,39 +1,54 @@
 import { useReducer } from "react"
 
+const ACTIONS ={
+    DEPOSIT:'deposit',
+    WITHDRAW :'withdraw',
+    RESET: 'reset'
+}
+
 // state = 500 initially defined
 // action = {type: 'DEPOSIT',payload:amount}
 
 const reducer =(state,action) => {
     switch(action.type){
-        case 'DEPOSIT':
-            return state +action.payload
-        case 'WITHDRAW':
-            return state -action.payload
+        case ACTIONS.DEPOSIT:
+            return {count:state.count +action.payload}
+        case ACTIONS.WITHDRAW:
+            return {count:state.count -action.payload}
+        case ACTIONS.RESET:
+            return {count:state.count = 500}
         default:
-            return state
+            return {count:state.count}
     }
 }
 
 export default function ComponentUseReducer(){
     const deposit = (amount) =>{
         dispatch({
-            type: 'DEPOSIT',
+            type: ACTIONS.DEPOSIT,
             payload:amount
         })
     }
     const withdraw = (amount)=>{
         dispatch({
-            type: 'WITHDRAW',
+            type: ACTIONS.WITHDRAW,
             payload: amount
         })
     }
+    const reset =()=>{
+        dispatch({
+            type: ACTIONS.RESET,
+            payload:null
+        })
+    }
 
-    const [amount,dispatch] = useReducer(reducer,500)
+    const [state,dispatch] = useReducer(reducer,{count: 500})
 
     return (
         <div>
-            <h1>{amount}</h1>
+            <h1>{state.count}</h1>
             <button onClick={()=>deposit(10)}>Deposit</button>
+            <button onClick={()=>reset()}>Reset</button>
             <button onClick={()=>withdraw(10)}>Withdraw</button>
         </div>
     )
